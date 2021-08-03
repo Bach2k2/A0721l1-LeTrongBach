c72b90f0388e661eca4eba8a024c3b78405593da
Bach2k2 <letrongbach02@gmail.com>
2021-08-03 15:15:19 +0700

    Tu bai 6-7

---
+++ module 1/bai_7_bien_va_kieu_du_lieu/thuc_hanh/SuDungToanTu.js
@@ -0,0 +1,12 @@
+let inputWidth;
+let inputHeight;
+inputWidth=prompt("Enter the width" );
+inputHeight=prompt("Enter the height");
+let width=parseInt(inputWidth);
+let height=parseInt(inputHeight);
+document.write("Enter width:<br>");
+document.write(width);
+document.write("<br>Enter height:<br>");
+document.write(height);
+let area=height*width;
+document.write('<br>The area is '+ area);
