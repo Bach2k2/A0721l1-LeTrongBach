67b135f9fb5b58be5305f978a3705a03fb623e9f
Bach2k2 <letrongbach02@gmail.com>
2022-03-19 17:02:50 +0700

    ss8.Validation

--- module_4/ss8_spring_validation/bai_tap/validate_info_music_app/src/main/java/com/example/exercise/controller/MusicController.java
+++ module_4/ss8_spring_validation/bai_tap/validate_info_music_app/src/main/java/com/example/exercise/controller/MusicController.java
@@ -1,8 +1,28 @@
 package com.example.exercise.controller;
 
+import com.example.exercise.model.Music;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MusicController {
 
+    @GetMapping()
+    public String showMusicSearch(Model model)
+    {
+        model.addAttribute("music",new Music());
+        return "search-music";
+    }
+
+    @PostMapping("/search")
+    public ModelAndView searchMusic(@ModelAttribute Music music)
+    {
+        ModelAndView model=new ModelAndView("result");
+        model.addObject("music",music);
+        return model;
+    }
 }
--- module_4/ss8_spring_validation/bai_tap/validate_info_music_app/src/main/java/com/example/exercise/model/Music.java
+++ module_4/ss8_spring_validation/bai_tap/validate_info_music_app/src/main/java/com/example/exercise/model/Music.java
@@ -12,19 +12,61 @@ public class Music {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotEmpty
-    @Max(800)
+    @NotEmpty(message = "Tên bài hát đang để bỏ trống")
+    @Max(value = 800,message = "Tên bài hát vượt quá độ dài quy định")
     @Pattern(regexp= "^[A-Z0-9a-z]+$",message = "Khong co ky tu dac biet")
     private String name;
 
-    @NotEmpty
-    @Max(300)
+    @NotEmpty(message ="Tên ca sĩ đang để trống" )
+    @Max(value = 300,message = "Tên ca sĩ vượt quá độ dài quy định ")
     @Pattern(regexp= "^[A-Z0-9a-z]+$",message = "Khong co ky tu dac biet")
     private String singer;
 
-    @NotEmpty
-    @Max(1000)
-    @@Pattern(regexp= "^[A-Z0-9a-z,]+$",message = "Khong co ky tu dac biet")
+    @NotEmpty(message = "Thể loại đang để trống")
+    @Max(value = 1000,message = "Thể loại vượt quá giá trị quy định")
+    @Pattern(regexp= "^[A-Z0-9a-z,]+$",message = "Khong co ky tu dac biet")
     private String category;
 
+    public Music() {
+    }
+
+    public Music(Long id, @NotEmpty(message = "Tên bài hát đang để bỏ trống") @Max(value = 800, message = "Tên bài hát vượt quá độ dài quy định") @Pattern(regexp = "^[A-Z0-9a-z]+$", message = "Khong co ky tu dac biet") String name, @NotEmpty(message = "Tên ca sĩ đang để trống") @Max(value = 300, message = "Tên ca sĩ vượt quá độ dài quy định ") @Pattern(regexp = "^[A-Z0-9a-z]+$", message = "Khong co ky tu dac biet") String singer, @NotEmpty(message = "Thể loại đang để trống") @Max(value = 1000, message = "Thể loại vượt quá giá trị quy định") @Pattern(regexp = "^[A-Z0-9a-z,]+$", message = "Khong co ky tu dac biet") String category) {
+        this.id = id;
+        this.name = name;
+        this.singer = singer;
+        this.category = category;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSinger() {
+        return singer;
+    }
+
+    public void setSinger(String singer) {
+        this.singer = singer;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
 }
